b35ea9fa359221601d50a6ff67e436a84b7b3af1
何东洲 <954504788@qq.com>
2018-06-18 21:57:03 +0800

    Initial Commit

---
+++ DesignCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DesignCode
+//
+//  Created by hdz on 2018/6/18.
+//  Copyright © 2018年 mobi.hdz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
